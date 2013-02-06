d156443ef98e263f29fbe3153d202025a08ba44a
Johnny Fuchs <johnny.fuchs@shoutlet.com>
2013-02-05 18:28:07 -0600

    moved js to it's own file, fixed some UI

--- header.php
+++ header.php
@@ -4,43 +4,14 @@
   <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
   <title><?php wp_title(' | ', true, 'right'); ?></title>
 
+  <!--
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  -->
 
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 
-  <script type="text/javascript">
-  $(function() {
-
-      var ssSelector = '.slideshow';
-      var ssHeight   = 300;
-      var ssWidth    = 400;
-
-      var nextSlide = function() {
-          var $current = $(ssSelector+' img.active');
-          if ( $current.length == 0 ) $current = $(ssSelector+' img:last');
-          var $next =  $current.next().length ? $current.next() : $(ssSelector+' img:first');
-          $current.addClass('prev-active');
-          $next.css({opacity: 0.0}).addClass('active').animate({opacity: 1.0}, 1000, function() { $current.removeClass('active prev-active'); });
-      }
-      var slideInt = setInterval(function(){
-          nextSlide();
-      }, 4000 );
-
-      // setup dimentions
-      $(ssSelector).css({
-        'min-height': ssHeight,
-        'max-height': ssHeight,
-        'min-width':  ssWidth,
-        'max-width':  ssWidth,
-        'overflow':  'hidden'
-      });
-      //$(ssSelector+' img').css({ });
-      $(ssSelector+' img').attr('width', ssWidth);
-      $(ssSelector+' img').removeAttr('height');
-      $(ssSelector+' img:first').addClass('active');
-  });
-  </script>
-
+  <script type="text/javascript" src="wp-content/themes/blankslate/scripts/jquery-1.7.min.js"></script>
+  <script type="text/javascript" src="wp-content/themes/blankslate/scripts/slideshow.js"></script>
 
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
   <!--[if gte IE 9]> <style type="text/css"> .radial-shadow { filter: none; } </style> <![endif]-->
