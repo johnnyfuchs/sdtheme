c222f0a4df8b8559b2354ff2c3c67ccbc96845ce
Johnny Fuchs <johnny.fuchs@shoutlet.com>
2013-02-18 00:06:11 -0600

    making headway with category view

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -27,3 +27,36 @@
       $(ssSelector+' img').removeAttr('height');
       $(ssSelector+' img:first').addClass('active');
   });
+
+
+$(function(){
+  pinterest_layout();
+});
+
+
+function pinterest_layout(){
+    var $pin_container = $('#pin-view');
+    var cols = [0, 0, 0];
+    var smallest = function( cols ) {
+        var lowest = Number.MAX_VALUE;
+        var index = 0;
+        var i = cols.length;
+        while (i--) {
+            if (cols[i] <= lowest) {
+                index = i;
+                lowest = cols[i];
+            }
+        }
+        return index;
+    }
+    for(i in cols){
+        $pin_container.prepend('<div class="pin-col"></div>');
+    }
+    $('.pin-item').each(function() {
+        var $this = $(this);
+        $this.find('.pin-img img').removeAttr('height').attr('width', '250px');
+        $this.append('<div class="clear"></div>');
+        $('.pin-col').eq(smallest(cols)).append($this);
+        cols[smallest(cols)] += $this.height();
+    });
+}
